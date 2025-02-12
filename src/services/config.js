99702f684c0dff9a43b1183906c9615afd19d647
Malcolm Mason Rodriguez <malcolmjmr@gmail.com>
2025-02-12 02:13:35 -0500

    .

--- src/services/config.js
+++ src/services/config.js
@@ -2,7 +2,7 @@ export const config = {
     apiKeys: {
         openai: '',
         anthropic: '',
-        gemini: 'AIzaSyD7VEhDccxLX8S_PChRf5U1PoxK7sa5NGU',
+        gemini: '',
         perplexity: ''
     },
 }
